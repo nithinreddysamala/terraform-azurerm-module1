c9862803073f430ec2fb8fa6d9d80d462a3c561e
nithinreddysamala <59999103+nithinreddysamala@users.noreply.github.com>
2020-08-05 15:19:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,169 @@
-#Azure Generic vNet Module
-data "azurerm_resource_group" "network" {
-  name = var.resource_group_name
+resource "azurerm_resource_group" "trf" {
+
+  name     = "${var.prefix}-Stbos"
+
+  location = var.location
+
 }
+
+ 
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  resource_group_name = data.azurerm_resource_group.network.name
-  location            = data.azurerm_resource_group.network.location
-  address_space       = [var.address_space]
-  dns_servers         = var.dns_servers
-  tags                = var.tags
+
+  name                = "${var.prefix}-network"
+
+  address_space       = ["13.0.0.0/16"]
+
+  location            = azurerm_resource_group.tfrg.location
+
+  resource_group_name = azurerm_resource_group.tfrg.name
+
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_names
-  resource_group_name  = data.azurerm_resource_group.network.name
-  address_prefix     = var.subnet_prefixes
+ 
+
+resource "azurerm_subnet" "tfsubnet" {
+
+  name                 = "StbosSN"
+
+  resource_group_name  = azurerm_resource_group.tfrg.name
+
   virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefix       = "13.0.2.0/24"
+
+}
+
+ 
+
+resource "azurerm_public_ip" "pip" {
+
+  name                = "${var.prefix}-pip"
+
+  resource_group_name = azurerm_resource_group.tfrg.name
+
+  location            = azurerm_resource_group.tfrg.location
+
+  allocation_method   = "Static"
+
+}
+
+ 
+
+resource "azurerm_network_interface" "tfnic" {
+
+  name                = "${var.prefix}-nic1"
+
+  resource_group_name = azurerm_resource_group.tfrg.name
+
+  location            = azurerm_resource_group.tfrg.location
+
+ 
+
+  ip_configuration {
+
+    name                          = "primary"
+
+    subnet_id                     = azurerm_subnet.tfsubnet.id
+
+    private_ip_address_allocation = "Static"
+
+    public_ip_address_id          = azurerm_public_ip.pip.id
+
+  }
+
+}
+
+ 
+
+resource "azurerm_network_security_group" "tfnsg" {
+
+  name                = "${var.prefix}-NSG"
+
+  location            = azurerm_resource_group.tfrg.location
+
+  resource_group_name = azurerm_resource_group.tfrg.name
+
+  security_rule {
+
+    access                     = "Allow"
+
+    direction                  = "Inbound"
+
+    name                       = "ssh"
+
+    priority                   = 100
+
+    protocol                   = "Tcp"
+
+    source_port_range          = "*"
+
+    source_address_prefix      = "*"
+
+    destination_port_range     = "22"
+
+    destination_address_prefix = "*"
+
+  }
+
+}
+
+ 
+
+resource "azurerm_network_interface_security_group_association" "nsgassociation" {
+
+  network_interface_id      = azurerm_network_interface.tfnic.id
+
+  network_security_group_id = azurerm_network_security_group.tfnsg.id
+
+}
+
+ 
+
+resource "azurerm_linux_virtual_machine" "main" {
+
+  name                            = "${var.prefix}-vm"
+
+  resource_group_name             = azurerm_resource_group.tfrg.name
+
+  location                        = azurerm_resource_group.tfrg.location
+
+  size                            = "Standard_DS1_v2"
+
+  admin_username                  = "adminuser"
+
+  admin_password                  = var.password
+
+  disable_password_authentication = false
+
+  network_interface_ids = [
+
+    azurerm_network_interface.tfnic.id,
+
+      ]
+
+ 
+
+  source_image_reference {
+
+    publisher = "Canonical"
+
+    offer     = "UbuntuServer"
+
+    sku       = "16.04-LTS"
+
+    version   = "latest"
+
+  }
+
+ 
+
+  os_disk {
+
+    storage_account_type = "Standard_LRS"
+
+    caching              = "ReadWrite"
+
+  }
+
 }
