4a4783ec8dba31084fecc91a025de486bea2bbd9
nithinreddysamala <59999103+nithinreddysamala@users.noreply.github.com>
2020-08-05 15:19:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,39 +1,19 @@
-variable "vnet_name" {
-  description = "Name of the vnet to create"
-  default     = "acctvnet"
+
+variable "prefix" {
+
+  description = "The prefix which should be used for all resources in this example"
+
 }
 
-variable "resource_group_name" {
-  description = "The name of an existing resource group to be imported."
-  default = "rg100"
+ 
+
+variable "location" {
+
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "central us"
+
 }
 
-variable "address_space" {
-  description = "The address space that is used by the virtual network."
-  default     = "10.0.0.0/16"
-}
+variable "password" {
 
-# If no values specified, this defaults to Azure DNS 
-variable "dns_servers" {
-  description = "The DNS servers to be used with vNet."
-  default     = []
-}
-
-variable "subnet_prefixes" {
-  description = "The address prefix to use for the subnet."
-  default     = "10.0.1.0/24"
-}
-
-variable "subnet_names" {
-  description = "A list of public subnets inside the vNet."
-  default     = "subnet1"
-}
-
-variable "tags" {
-  description = "The tags to associate with your network and subnets."
-  type        = map(string)
-
-  default = {
-    environment = "dev"
-  }
 }
